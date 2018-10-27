ed4cd5e5681f44feb9319e8bd745294b6948bfeb
Pierre <jpconstantineau@gmail.com>
2018-10-26 23:53:09 -0600

    adding NFC pins to GPIO conversion code and 4x4x4x4Backpack board

--- firmware/firmware_main.cpp
+++ firmware/firmware_main.cpp
@@ -119,6 +119,22 @@ uint8_t mvToPercent(float mvolts) {
 /**************************************************************************************************************************/
 void setup() {
 
+      // this code enables the NFC pins to be GPIO.
+     if ((NRF_UICR->NFCPINS & UICR_NFCPINS_PROTECT_Msk) == (UICR_NFCPINS_PROTECT_NFC << UICR_NFCPINS_PROTECT_Pos)){
+       // Serial.println("Fix NFC pins");
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
+        NRF_UICR->NFCPINS &= ~UICR_NFCPINS_PROTECT_Msk;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy);
+        //Serial.println("Done");
+        delay(500);
+        NVIC_SystemReset();
+      } // end of NFC switch code.
+
+      
+
   Serial.begin(115200);
 
   LOG_LV1("BLEMIC","Starting %s" ,DEVICE_NAME);
