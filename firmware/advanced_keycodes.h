a00056082a67e645f305787b1a99a7eaad334df4
Julian Komaromy <julian.komaromy@gmail.con>
2018-11-01 10:17:35 +0100

    changed layercodes

--- firmware/advanced_keycodes.h
+++ firmware/advanced_keycodes.h
@@ -3,7 +3,7 @@
 #ifndef ADVANCED_KEYCODES_H
 #define ADVANCED_KEYCODES_H
 
-#define TG(layer) (((uint16_t) layer << 8) | 0x0000)
-#define MO(layer) (((uint16_t) layer << 8) | 0x0001)
+#define TG(layer) (((uint16_t) layer << 8) | 0x0001)
+#define MO(layer) (((uint16_t) layer << 8) | 0x0000)
 
 #endif
