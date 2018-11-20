00308fa960305ae22978423bd30b0102c6f90b6d
Julian Komaromy <julian.komaromy@gmail.con>
2018-11-20 20:41:03 +0100

    fix indentation in airgodox config

--- firmware/keyboards/airgodox/keymaps/default/keymap.h
+++ firmware/keyboards/airgodox/keymaps/default/keymap.h
@@ -20,59 +20,81 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 #include "keyboard_config.h"
 #include "advanced_keycodes.h"
 
-#define KC_CAP_D MOD(MOD_LSHIFT, KC_D)
 #ifndef KEYMAP_H
 #define KEYMAP_H
 
+#define KC_CAP_D MOD(MOD_LSHIFT, KC_D)
+#define _QWERTY 0
+#define _L1  1
+#define _PRESS 0
+#define _MT_TAP 1
+#define _MT_HOLD 2
+#define _DT_TAP 3
+#define _DT_DOUBLETAP 4
 
-    #define _QWERTY 0
-    #define _L1  1
+#if KEYBOARD_SIDE == LEFT
 
-    #if KEYBOARD_SIDE == LEFT
+const uint32_t keymaps[][5][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = {
+        [_PRESS] = KEYMAP(
+            KC_ESC,    KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,
+            KC_TAB,    KC_A,    KC_S,    KC_CAP_D, KC_F,    KC_G,
+            KC_LSHIFT, MD(MD_DT_TAP, KC_Z), MD(MD_DT_DOUBLETAP, KC_X),MD(MD_MT_TAP, KC_C),MD(MD_MT_HOLD, KC_V),     KC_B,
+            KC_NO,     KC_NO,   KC_NO,   LAYER_1,  KC_LCTRL, KC_LGUI),
+        [_MT_TAP] = {
+            [0] = {},
+            [1] = {},
+            [2] = {KC_NO, KC_NO, KC_NO, KC_D, KC_NO, KC_NO},
+            [3] = {}
+        },
+        [_MT_HOLD] = {
+            [0] = {},
+            [1] = {},
+            [2] = {KC_NO, KC_NO, KC_NO, KC_NO, KC_F, KC_NO},
+            [3] = {}
+        },
+        [_DT_TAP] = {
+            [0] = {},
+            [1] = {},
+            [2] = {KC_NO, KC_A, KC_NO, KC_NO, KC_NO, KC_NO},
+            [3] = {}
+        },
+        [_DT_DOUBLETAP] = {
+            [0] = {},
+            [1] = {},
+            [2] = {KC_NO, KC_NO, KC_S, KC_NO, KC_NO, KC_NO},
+            [3] = {}
+        }
+    },
 
-        const uint32_t keymaps[][MATRIX_METHODS][MATRIX_ROWS][MATRIX_COLS] = {
-            [_QWERTY] = {
-                [MD_PRESS] = KEYMAP(
-                    KC_ESC,    KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,
-                    KC_TAB,    KC_A,    KC_S,    KC_CAP_D, KC_F,    KC_G,
-                    KC_LSHIFT, KC_Z,    KC_X,    MD(MD_MT_TAP, KC_C),     KC_V,     KC_B,
-                    KC_NO,     KC_NO,   KC_NO,   LAYER_1,  KC_LCTRL, KC_LGUI),
-                [1] = {
-                    [0] = {},
-                    [1] = {},
-                    [2] = {KC_NO, KC_NO, KC_NO, KC_B, KC_NO, KC_NO}
-                }
-            },
+    [_L1] = {
+        [MD_PRESS] = KEYMAP(
+            KC_GRV,    KC_1,    KC_2,     KC_3,    KC_4,     KC_5,
+            KC_CAPS,   KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5,
+            KC_LSHIFT, KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,
+            KC_NO,     KC_NO,   KC_NO,    LAYER_0, KC_LCTRL, KC_LGUI)
+    }
+};
 
-            [_L1] = {
-                [MD_PRESS] = KEYMAP(
-                    KC_GRV,    KC_1,    KC_2,     KC_3,    KC_4,     KC_5,
-                    KC_CAPS,   KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5,
-                    KC_LSHIFT, KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,
-                    KC_NO,     KC_NO,   KC_NO,    LAYER_0, KC_LCTRL, KC_LGUI)
-            }
-        };
+#else
 
-    #else
+const uint32_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-        const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = {
+        [MD_PRESS] = KEYMAP(
+            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
+            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,KC_ENTER,
+            KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH,KC_RALT,
+            KC_RSFT, KC_SPC,  LAYER_1, KC_NO,   KC_NO,   KC_NO)
+    },
 
-            [_QWERTY] = {
-                [MD_PRESS] = KEYMAP(
-                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
-                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,KC_ENTER,
-                    KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH,KC_RALT,
-                    KC_RSFT, KC_SPC,  LAYER_1, KC_NO,   KC_NO,   KC_NO)
-            },
-
-            [_L1] = {
-                KEYMAP(
-                    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-                    KC_LBRC, KC_RBRC, KC_BSLS, KC_UP,  KC_QUOT, KC_EQL,
-                    KC_F11,  KC_F12,  KC_LEFT, KC_DOWN,KC_RGHT, _______,
-                    KC_RSFT, KC_APP,  LAYER_0, KC_NO,  KC_NO,   KC_NO )
-            }
-        };
-    #endif
-
-#endif
+    [_L1] = {
+        KEYMAP(
+            KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
+            KC_LBRC, KC_RBRC, KC_BSLS, KC_UP,  KC_QUOT, KC_EQL,
+            KC_F11,  KC_F12,  KC_LEFT, KC_DOWN,KC_RGHT, _______,
+            KC_RSFT, KC_APP,  LAYER_0, KC_NO,  KC_NO,   KC_NO )
+    }
+};
+#endif /* KEYBOARD_SIDE */
+#endif /* KEYMAP_H */
