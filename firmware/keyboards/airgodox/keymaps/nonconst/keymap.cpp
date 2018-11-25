52d93b0535a33071aff9e2cc62eb61760d2ae41a
Julian Komaromy <julian.komaromy@gmail.con>
2018-11-25 10:15:33 +0100

    update agdx configs for testing

--- firmware/keyboards/airgodox/keymaps/nonconst/keymap.cpp
+++ firmware/keyboards/airgodox/keymaps/nonconst/keymap.cpp
@@ -20,14 +20,14 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 #if KEYBOARD_SIDE == LEFT
 
 /*
- * initiialize the default layer (QWERTY/Press) with the following
+ * initiialize the default layer (QWERTY/PRESS) with the following
  * keymap
  */
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {{
         {KC_ESC,    KC_Q,    KC_W,    KC_E,     KC_R,     KC_T},
-        {KC_A,      KC_NO,    KC_S,    KC_CAP_D, KC_F,     KC_G},
-        {KC_LSHIFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B},
+        {KC_TAB,      KC_NO,    KC_S,    KC_CAP_D, KC_F,     KC_G},
+        {KC_LSHIFT, TG(KC_LSHIFT),    KC_X,    KC_C,     KC_V,     KC_B},
         {KC_NO,     KC_NO,   KC_NO,   LAYER_1,  KC_LCTRL, KC_LGUI}
     }};
 
@@ -41,7 +41,7 @@ void setupKeymap()
             KC_GRV,    KC_1,    KC_2,     KC_3,    KC_4,     KC_5,
             KC_CAPS,   KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5,
             KC_LSHIFT, KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,
-            KC_NO,     KC_NO,   KC_NO,    LAYER_0, KC_LCTRL, KC_LGUI);
+            KC_NO,     KC_NO,   KC_NO,    LAYER_1, KC_LCTRL, KC_LGUI);
 
     /*
      * add the other layers
@@ -63,27 +63,10 @@ void setupKeymap()
 }
 
 #else
- 
-void setupKeymap() 
-{
-    uint32_t keycodes[][5][MATRIX_ROWS][MATRIX_COLS] = {
-        [_QWERTY] = {
-            [MD_PRESS] = KEYMAP(
-                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
-                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,KC_ENTER,
-                KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH,KC_RALT,
-                KC_RSFT, KC_SPC,  LAYER_1, KC_NO,   KC_NO,   KC_NO)
-        },
 
-        [_L1] = {
-            KEYMAP(
-                KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-                KC_LBRC, KC_RBRC, KC_BSLS, KC_UP,  KC_QUOT, KC_EQL,
-                KC_F11,  KC_F12,  KC_LEFT, KC_DOWN,KC_RGHT, _______,
-                KC_RSFT, KC_APP,  LAYER_0, KC_NO,  KC_NO,   KC_NO )
-        }
-    };
-}
+/*
+ * TODO: configure right side
+ */
 
 #endif /* KEYBOARD_SIDE */
 
