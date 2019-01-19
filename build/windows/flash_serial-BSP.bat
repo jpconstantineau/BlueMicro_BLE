aa04c9fbdcdfbba080cb112ea8ce405cfb262a27
Pierre <jpconstantineau@gmail.com>
2019-01-19 16:37:44 -0600

    re-org of Build tool folders & Iris ROW_PINS edit.

--- build/windows/flash_serial-BSP.bat
+++ build/windows/flash_serial-BSP.bat
@@ -5,13 +5,13 @@ if ["%~2"]==[""] goto usage
 if ["%~3"]==[""] goto usage
 if ["%~4"]==[""] goto usage
 
-if exist ..\output\%~1\%~1-%~2-%~3.zip (
+if exist ..\..\output\%~1\%~1-%~2-%~3.zip (
 	goto flash
 ) else (
 	@echo Compiled zip package not found: running build script first.
 	powershell ./build-windows-BSP.ps1 %~1 %~2 %~3
 
-	if exist ..\output\%~1\%~1-%~2-%~3.zip (
+	if exist ..\..\output\%~1\%~1-%~2-%~3.zip (
 		goto flash
 	) else (
 		@echo Compiled zip package still not found: do you have the right keyboard, keymap and target?
@@ -25,7 +25,7 @@ if exist ..\output\%~1\%~1-%~2-%~3.zip (
 
 :flash
    	echo Flashing %~1-%~2-%~3 over serial port %~4
-	C:\Users\pierre\Documents\Arduino\hardware\Adafruit\Adafruit_nRF52_Arduino\tools\adafruit-nrfutil\win32\adafruit-nrfutil.exe --verbose dfu serial -pkg ..\output\%~1\%~1-%~2-%~3.zip -p %~4 -b 115200 --singlebank
+	C:\Users\pierre\Documents\Arduino\hardware\Adafruit\Adafruit_nRF52_Arduino\tools\adafruit-nrfutil\win32\adafruit-nrfutil.exe --verbose dfu serial -pkg ..\..\output\%~1\%~1-%~2-%~3.zip -p %~4 -b 115200 --singlebank
 
 
 :usage
