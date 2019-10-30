19843ec0ef983e1fc507dec249eeb364d3dd6b74
Pierre <jpconstantineau@gmail.com>
2019-10-30 16:43:01 -0600

    updating BSP to windows flash files

--- build/windows/bootloader.bat
+++ build/windows/bootloader.bat
@@ -1,1 +1,1 @@
-nrfjprog.exe --program %localappdata%\Arduino15\packages\adafruit\hardware\nrf52\0.11.1\bootloader\feather_nrf52832\feather_nrf52832_bootloader-0.2.11_s132_6.1.1.hex -f nrf52 --chiperase --reset 
+nrfjprog.exe --program %localappdata%\Arduino15\packages\adafruit\hardware\nrf52\0.14.6\bootloader\feather_nrf52832\feather_nrf52832_bootloader-0.2.13_s132_6.1.1.hex -f nrf52 --chiperase --reset 
