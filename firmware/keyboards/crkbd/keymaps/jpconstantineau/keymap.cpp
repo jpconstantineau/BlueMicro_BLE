8295a804f2a07b0d99dea2ab8a6442278988a6fb
Pierre <jpconstantineau@gmail.com>
2020-04-28 22:41:52 -0600

    fixing some more issues with duplicate breaks

--- firmware/keyboards/crkbd/keymaps/jpconstantineau/keymap.cpp
+++ firmware/keyboards/crkbd/keymaps/jpconstantineau/keymap.cpp
@@ -381,15 +381,12 @@ void process_user_macros(uint16_t macroid)
       break;
      case LARW_R:
      addStringToQueue("->");
-      break;
      break;
      case IPADDR:
      addStringToQueue("192.168.1.");
-      break;
      break;
      case SMILE:
      addStringToQueue(":)");
-      break;
      break;
      case IPSUM:
      addStringToQueue("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
