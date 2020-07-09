4b36e7340a8a65e83b974e4f8c0e4d001f6db80d
Pierre <jpconstantineau@gmail.com>
2020-07-08 23:17:01 -0600

    troubleshooting github docker actions

--- build/docker/nrf52xxx.sh
+++ build/docker/nrf52xxx.sh
@@ -22,7 +22,7 @@ arduinoDataPath=$(cd ~/.arduino15 & pwd)
 nrf52PackagePath="/home/$USER/.arduino15/packages/adafruit/hardware/nrf52"
 
 scriptPath="$(dirname "$BASH_SOURCE")"
-
+echo $scriptPath
 #replace this variable with path to your avr installation
 arduinoAvrPath="$arduinoPath/hardware/arduino/avr"
 
