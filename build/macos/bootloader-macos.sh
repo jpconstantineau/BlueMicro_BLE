fe837ae1fb1f0ce67a6fe54f1d0e45f5a378901e
Pierre <jpconstantineau@gmail.com>
2020-10-16 21:28:52 -0600

    potential fix for macos build script to build on new folder structure

--- build/macos/bootloader-macos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd -- "$(dirname "$BASH_SOURCE")"
-
-arduinoDataPath=$(cd ~/Library/Arduino15 && pwd)
-nrf52PackagePath="$arduinoDataPath/packages/adafruit/hardware/nrf52"
-
-nrfjprog --program ${nrf52PackagePath}/0.18.0/bootloader/feather_nrf52832/feather_nrf52832_bootloader-0.3.0_s132_6.1.1.hex -f nrf52 --chiperase --reset
