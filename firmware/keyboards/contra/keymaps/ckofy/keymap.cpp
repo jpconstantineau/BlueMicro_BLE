7782e536479cc610e13afc510882e1831c92878f
ckofy <kolbunov@gmail.com>
2020-10-17 23:36:55 -0500

    personal contra keymap changed

--- firmware/keyboards/contra/keymaps/ckofy/keymap.cpp
+++ firmware/keyboards/contra/keymaps/ckofy/keymap.cpp
@@ -17,37 +17,59 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 */
 
 #include "keymap.h"
- std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
+ 
+std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {{
         {KC_TAB,    KC_Q,    KC_W,    KC_F,   KC_P,    KC_B,    KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN,  KC_BSPACE,},
-        {KC_BSPACE, KC_A,    KC_R,    KC_S,   KC_T,    KC_G,    KC_M,    KC_N,   KC_E,    KC_I,    KC_O,     KC_QUOT,},
-        {KC_ESC,    KC_Z,    KC_X,    KC_C,   KC_D,    KC_V,    KC_K,    KC_H,   KC_COMMA,KC_DOT,  KC_SLASH, KC_ENTER,},
-        {KC_LCTL,   KC_LALT, KC_LGUI, KC_LCTL,LAYER_1, KC_SPC,  KC_LSFT, LAYER_2,KC_LEFT, KC_UP,   KC_DOWN,  KC_RIGHT}
+        {L_MACRO,   KC_A,    KC_R,    KC_S,   KC_T,    KC_G,    KC_M,    KC_N,   KC_E,    KC_I,    KC_O,     KC_QUOT,},
+        {SHESC,     KC_Z,    KC_X,    KC_C,   KC_D,    KC_V,    KC_K,    KC_H,   KC_COMMA,KC_DOT,  KC_SLASH, KC_ENTER,},
+        {KC_LCTL,   KC_LSFT, KC_LALT, KC_LGUI,L_LOWER, KC_SPC,OS(KC_LSFT),L_RAISE,KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT}
     }};
 
  
 void setupKeymap() {
 
-    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP(
-        KC_TAB,    _______, _______, _______,KC_CAPS, _______, KC_PGUP, KC_HOME,KC_UP,   KC_END,  KC_DEL,   KC_BSPACE,  
-        KC_BSPC,   KC_LGUI, KC_LALT, KC_LSFT,KC_LCTL, KC_SVE,  KC_PGDN, KC_LEFT,KC_DOWN, KC_RGHT, KC_BSPC,  KC_QUOTE,
-        KC_ESC,    KC_UND,  KC_CUT,  KC_CPY, KC_PST,  _______, KC_INS , _______,KC_COMMA,KC_DOT,  KC_SLSH,  KC_ENT, 
-        _______,   _______, _______, LAYER_3,LAYER_1, KC_SPC,  KC_SPC,  LAYER_2,_______, _______, _______,  _______);
+    uint32_t colemak_taph[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP( 
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+  SHESC_TAP,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX);
 
-    uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP(
-        KC_TAB,    KC_AMPR, KC_PERC, KC_HASH,KC_LPRN, KC_EXLM, KC_ASTR, KC_RPRN, KC_AT,   KC_DLR, KC_CIRC,KC_BSPACE,  
-        KC_BSPC,   KC_7,    KC_5,    KC_3,   KC_1,    KC_9,    KC_8,    KC_0,    KC_2,    KC_4,   KC_6,   KC_BSLS,
-        KC_LSFT,   KC_GRAVE,KC_LBRC, KC_RBRC,KC_UNDS, KC_EQL,  KC_PLUS, KC_MINUS,KC_COMMA,KC_DOT, KC_SLSH,KC_ENT, 
-        _______,   _______, _______, _______,LAYER_1, KC_SPC,  KC_LSFT, LAYER_2, _______, _______,_______,_______);
+    uint32_t colemak_hold[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP( 
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+  SHESC_HOLD,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX);
 
-    uint32_t layer3[MATRIX_ROWS][MATRIX_COLS] =
+    uint32_t lower[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP(
-        KC_GRAVE,KC_F1,  KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,  _______,  
-        _______, _______,_______,_______, _______, _______, _______,  _______,  _______,  KC_F11,   KC_F12,  _______,
-        _______, _______,_______,_______, _______, _______, _______,  _______,  _______,  _______,  _______, _______, 
-        _______, _______,_______,_______, _______, _______, _______,  _______,  _______,  _______,  _______, _______);
+    _______,   KC_LOCK, XXXXXXX, KC_SWP, KC_CAPS, KC_BRK,  KC_PGUP, KC_HOME,KC_UP,   KC_END,  KC_DEL,   _______,  
+    _______,   KC_LGUI, KC_LALT, KC_LSFT,KC_LCTL, KC_SVE,  KC_PGDN, KC_LEFT,KC_DOWN, KC_RGHT, KC_BSPC,  _______,
+    _______,   KC_UND,  KC_CUT,  KC_CPY, KC_PST,  KC_DUP,  KC_INS , XXXXXXX,KC_COMMA,KC_DOT,  KC_SLSH,  _______, 
+    _______,   _______, _______, _______,_______, _______, _______, _______,_______, _______, _______,  _______);
+
+    uint32_t raise[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
+    _______,   KC_AMPR, KC_PERC, KC_HASH,KC_LPRN, KC_EXLM, KC_ASTR, KC_RPRN, KC_AT,   KC_DLR, KC_CIRC,_______,  
+    _______,   KC_7,    KC_5,    KC_3,   KC_1,    KC_9,    KC_8,    KC_0,    KC_2,    KC_4,   KC_6,   KC_BSLS,
+    _______,   KC_GRAVE,KC_LBRC, KC_RBRC,KC_UNDS, KC_EQL,  KC_PLUS, KC_MINUS,KC_COMMA,KC_DOT, KC_SLSH,_______, 
+    _______,   _______, _______, _______,_______, _______, _______, _______, _______, _______,_______,_______);
+	
+    uint32_t adjust[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP(
+    KC_GRAVE,KC_F1,  KC_F2,  KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,  _______,  
+    _______, _______,_______,_______, _______, _______, _______,KM_COLEMAK,KM_QWERTY, KC_F11,   KC_F12,  _______,
+    _______, _______,_______,_______, _______, _______, _______,  _______,  _______,  _______,  _______, _______, 
+    _______, _______,_______,_______, _______, _______, _______,  _______,  _______,  _______,  _______, _______);
+	
+	uint32_t macro[MATRIX_ROWS][MATRIX_COLS] =
+        KEYMAP( 
+    PRINT_BATTERY,HOME_ADD,EMAIL_1,EMAIL_2,WORK_ADD,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+    PRINT_INFO,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX);
 	
     /*
      * add the other layers
@@ -56,11 +78,70 @@ void setupKeymap() {
     {
         for (int col = 0; col < MATRIX_COLS; ++col)
         {
-            matrix[row][col].addActivation(_L1, Method::PRESS, layer1[row][col]);
-            matrix[row][col].addActivation(_L2, Method::PRESS, layer2[row][col]);
-            matrix[row][col].addActivation(_L3, Method::PRESS, layer3[row][col]);
+            matrix[row][col].addActivation(_COLEMAK, Method::MT_TAP, colemak_taph[row][col]);
+            matrix[row][col].addActivation(_COLEMAK, Method::MT_HOLD, colemak_hold[row][col]);			
+            matrix[row][col].addActivation(_LOWER, Method::PRESS, lower[row][col]);
+            matrix[row][col].addActivation(_RAISE, Method::PRESS, raise[row][col]);
+            matrix[row][col].addActivation(_ADJUST, Method::PRESS, adjust[row][col]);
+            matrix[row][col].addActivation(_MACRO,  Method::PRESS, macro[row][col]);
         }
     }
 
 }
 
+void process_user_macros(uint16_t macroid)
+{
+
+uint32_t colemak[MATRIX_ROWS][MATRIX_COLS] =
+    KEYMAP(    
+        KC_TAB,    KC_Q,    KC_W,    KC_F,   KC_P,    KC_B,    KC_J,    KC_L,   KC_U,    KC_Y,    KC_SCLN,  KC_BSPACE,
+        L_MACRO,   KC_A,    KC_R,    KC_S,   KC_T,    KC_G,    KC_M,    KC_N,   KC_E,    KC_I,    KC_O,     KC_QUOT,
+        KC_ESC,    KC_Z,    KC_X,    KC_C,   KC_D,    KC_V,    KC_K,    KC_H,   KC_COMMA,KC_DOT,  KC_SLASH, KC_ENTER,
+        KC_LCTL,   KC_LSFT, KC_LALT, KC_LGUI,L_LOWER, KC_SPC,OS(KC_LSFT),L_RAISE,KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT);
+
+uint32_t qwerty[MATRIX_ROWS][MATRIX_COLS] =
+    KEYMAP(    
+        KC_ESC,    KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,     KC_BSPACE,
+        KC_TAB,    KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+        KC_LSFT,   KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,  KC_COMMA,KC_DOT,  KC_SLASH, KC_ENTER,
+        KC_LCTL,   KC_LGUI, KC_LALT, KC_RGUI,L_LOWER, KC_SPC,  KC_SPC, L_RAISE,KC_LEFT, KC_UP,   KC_DOWN,  KC_RIGHT);						  
+	  
+ switch ((macroid))
+ { 
+     case (KM_COLEMAK):
+        for (int row = 0; row < MATRIX_ROWS; ++row)
+        {
+            for (int col = 0; col < MATRIX_COLS; ++col)
+            {
+                matrix[row][col].addActivation(_COLEMAK, Method::PRESS, colemak[row][col]);
+            }
+        }
+     break;
+     case (KM_QWERTY):
+        for (int row = 0; row < MATRIX_ROWS; ++row)
+        {
+            for (int col = 0; col < MATRIX_COLS; ++col)
+            {
+                matrix[row][col].addActivation(_QWERTY, Method::PRESS, qwerty[row][col]);
+            }
+        }
+     break; 
+     case HOME_ADD:
+     addStringToQueue("123 Quiet Crescent");
+     break;
+     case WORK_ADD:
+     addStringToQueue("123 Work Place");
+      break;
+     case EMAIL_1:
+     addStringToQueue("Primary@Email");
+      break;
+     case EMAIL_2:
+     addStringToQueue("Other@Email");
+      break;   
+ }
+}
+
+void process_user_layers(uint16_t layermask)
+{
+    KeyScanner::process_for_tri_layers(_LOWER, _RAISE, _ADJUST);
+}
