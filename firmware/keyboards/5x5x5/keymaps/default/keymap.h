4518921635b4b7965e11029338309de1d84e0947
Pierre <jpconstantineau@gmail.com>
2021-01-21 20:40:46 -0600

    initial 5x5x5 default keymap

--- firmware/keyboards/5x5x5/keymaps/default/keymap.h
+++ firmware/keyboards/5x5x5/keymaps/default/keymap.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 <Pierre Constantineau>
+Copyright 2018 <Pierre Constantineau>
 
 3-Clause BSD License
 
@@ -17,40 +17,22 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
-#ifndef HARDWARE_CONFIG_H
-#define HARDWARE_CONFIG_H
-#include "hardware_variants.h"
+#include <stdint.h>
+#include "hid_keycodes.h"
+#include "keyboard_config.h"
+#include "advanced_keycodes.h"
+#include "Key.h"
+#include <array>
+#ifndef KEYMAP_H
+#define KEYMAP_H
 
 
-/* HARDWARE DEFINITION*/
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
+#define NUM_LAYERS 2
 
-/* Column  pin configuration
- * col:  0  1  2  3    4  5  6    7  8  9  10 11 12 13 14
- * pin:  D7 E6 B4 B5   B6 B7 D6   F7 F6 F5 F4 F1 F0 B3 B1
- * GPIO: 6  5  8  1.09 4  12 1.06 24 22 13 15 ?? ?? 9  10
- */
-/* Row  pin configuration
- * row:  0    1  2  3  4   
- * pin:  B2   D1 D0 D4 C6
- * GPIO: 1.11 30 2  29 26
- */
+#define _L0 0
+#define _L1  1
 
-#define MATRIX_ROW_PINS {43,30,2,29,26} 
-#define MATRIX_COL_PINS {6,5,8,41,4,12,38,24,22,13,15,17,20,9,10} 
+void setupKeymap();
+extern std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix;
 
-#define UNUSED_PINS {}
-
-/* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION COL2ROW
-        #define BATTERY_TYPE BATT_LIPO
-        #define VBAT_PIN  31
-        #define VCC_PIN 32
-#define VCC_POLARITY_ON 1
-
-#define  STATUS_BLE_LED_PIN  42  //blue = 1.10
-#define  STATUS_KB_LED_PIN 46  //red = 1.04
-
-#endif /* HARDWARE_CONFIG_H */
+#endif /* KEYMAP_H */
