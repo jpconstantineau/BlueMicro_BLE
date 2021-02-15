a0e1b128e6413abf8f99e99fed418f6e4456a30f
Pierre <jpconstantineau@gmail.com>
2021-02-15 09:29:35 -0600

    initial commit for BlueZero and keypad-zero

--- firmware/keyboards/keypad-zero/keymaps/default/keymap.h
+++ firmware/keyboards/keypad-zero/keymaps/default/keymap.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2020 <Pierre Constantineau>
+Copyright 2018 <Pierre Constantineau>
 
 3-Clause BSD License
 
@@ -17,25 +17,22 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
+#include <stdint.h>
+#include "hid_keycodes.h"
+#include "keyboard_config.h"
+#include "advanced_keycodes.h"
+#include "Key.h"
+#include <array>
+#ifndef KEYMAP_H
+#define KEYMAP_H
 
-#ifndef HARDWAREVARIANTS_H
-#define HARDWAREVARIANTS_H
-#include "datastructures.h"
+#define _L0  0
+#define _L1  1
+#define _L2  2
 
-// putting this here since it's called in every keyboard_config and we need this for all the keymaps.
-// THIS FILE CONTAINS THE MACRO DEFINITIONS THAT USERS MAY USE IN THEIR keymap.cpp/h FILES.
-// FOR DATASTRUCTURES USED BY USER FUNCTIONS, SEE datastructures.h
+void setupKeymap();
+extern std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix;
 
-#define NODIODES      0
-#define COL2ROW       0  // TODO: add automatic selection of ANTI-GHOSTING...
-#define ROW2COL       1
+#endif
 
-#define TEST 0
-#define LEFT 1
-#define RIGHT 2
-#define SINGLE 3
 
-#define KEYMAP2ARRAY(OTHERMACROHERE) {OTHERMACROHERE}
-#define ADDLAYER(LAYER_INPUT,METHOD_INPUT,KEYMAP_INPUT )  for (int row = 0; row < MATRIX_ROWS; ++row) { for (int col = 0; col < MATRIX_COLS; ++col){ matrix[row][col].addActivation(LAYER_INPUT, METHOD_INPUT, KEYMAP_INPUT[row][col]);}}
-#define _PINNUM(port, pin) ((port)*32 + (pin))
-#endif  /*HARDWAREVARIANTS_H*/
