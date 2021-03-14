cba07dbb81f9226c5663a4e625e29c6876df723f
Pierre <jpconstantineau@gmail.com>
2021-03-14 09:44:15 -0600

    adding BlueHand test keymap

---
+++ firmware/keyboards/bluehand/hardware/pca10056/bluehand/hardware_config.h
@@ -0,0 +1,36 @@
+/*
+Copyright 2020-2021<Pierre Constantineau>
+
+3-Clause BSD License
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+#ifndef HARDWARE_CONFIG_H
+#define HARDWARE_CONFIG_H
+#include "hardware_variants.h"
+/* HARDWARE DEFINITION*/
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 8
+#define MATRIX_ROW_PINS { 33 }
+#define MATRIX_COL_PINS { 29, 2, 28, 3, 10, 9, 24, 13}
+#define UNUSED_PINS {}
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+#define BATTERY_TYPE BATT_CR2032
+
+#define SPEAKER_PIN 10
+#endif /* HARDWARE_CONFIG_H */
