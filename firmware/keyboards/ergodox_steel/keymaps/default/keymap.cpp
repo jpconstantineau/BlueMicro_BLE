6dd5f7cdd9e977ceafe370b6e333dfd41f8095e3
Matt Mills <sunrisetimes@gmail.com>
2021-07-13 00:20:53 -0700

    paste in keymaps of the correct size

--- firmware/keyboards/ergodox_steel/keymaps/default/keymap.cpp
+++ firmware/keyboards/ergodox_steel/keymaps/default/keymap.cpp
@@ -23,87 +23,82 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
  * initiialize the default layer (QWERTY/PRESS) with the following
  * keymap
  */
-std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-    {{
-        {KC_ESC,    KC_Q,    KC_W,    KC_E,     KC_R,     KC_T},
-        {KC_TAB,      KC_NO,    KC_S,    KC_CAP_D, KC_F,     KC_G},
-        {KC_LSHIFT, TG(KC_LSHIFT),    KC_X,    KC_C,     KC_V,     KC_B},
-        {KC_NO,     KC_NO,   KC_NO,   LAYER_1,  KC_LCTRL, KC_LGUI}
-    }};
+ std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
+     {KEYMAP( /* qwerty */
+         KC_EQL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   \
+         KC_TAB,  _______, KC_F12, _______, _______, _______,  _______,  \
+         KC_LCTL,  _______, _______, _______, _______, _______,  _______, \
+         KC_LSFT,  _______, _______, _______, _______, _______,   KC_DEL,  \
+         KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+         KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+       )
+     )};
 
-/*
- * add extra layers or single keys to this function 
- */
-void setupKeymap() 
-{
-    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP(
-            KC_GRV,    KC_1,    KC_2,     KC_3,    KC_4,     KC_5,
-            KC_CAPS,   KC_F1,   KC_F2,    KC_F3,   KC_F4,    KC_F5,
-            KC_LSHIFT, KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,
-            KC_NO,     KC_NO,   KC_NO,    LAYER_1, KC_LCTRL, KC_LGUI);
+ void setupKeymap() {
 
-    /*
-     * add the other layers
-     */
-    for (int row = 0; row < MATRIX_ROWS; ++row)
-    {
-        for (int col = 0; col < MATRIX_COLS; ++col)
-        {
-            matrix[row][col].addActivation(_L1, Method::PRESS, layer1[row][col]);
-        }
-    }
+     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
+     KEYMAP(
+       KC_EQL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   \
+       KC_TAB,  _______, KC_F12, _______, _______, _______,  _______,  \
+       KC_LCTL,  _______, _______, _______, _______, _______,  _______, \
+       KC_LSFT,  _______, _______, _______, _______, _______,   KC_DEL,  \
+       KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+       KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+     );
 
-    /* 
-     * add special, single activations with the 
-     * layer, activation method and activation
-     */
-    matrix[2][3].addActivation(_QWERTY, Method::MT_TAP, KC_Y);
-    matrix[1][1].addActivation(_QWERTY, Method::MT_TAP, TG(KC_LSHIFT));
-}
+     /*
+      * add the other layers
+      */
+     for (int row = 0; row < MATRIX_ROWS; ++row)
+     {
+         for (int col = 0; col < MATRIX_COLS; ++col)
+         {
+             matrix[row][col].addActivation(_L1, Method::PRESS, layer1[row][col]);
+         }
+     }
 
+ }
 #else
 
 /*
  * TODO: configure right side
  */
 
-std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
-    {{
-        {KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,},
-        {KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,KC_ENTER,},
-        {KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH,KC_RALT,},
-        {KC_RSFT, KC_SPC,  LAYER_1, KC_NO,   KC_NO,   KC_NO}
-    }};
+ std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
+     {KEYMAP( /* qwerty */
+         KC_EQL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   \
+         KC_TAB,  _______, KC_F12, _______, _______, _______,  _______,  \
+         KC_LCTL,  _______, _______, _______, _______, _______,  _______, \
+         KC_LSFT,  _______, _______, _______, _______, _______,   KC_DEL,  \
+         KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+         KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+       )
+     )};
 
-void setupKeymap() 
-{
-    uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
-        KEYMAP(
-           KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,
-            KC_LBRC, KC_RBRC, KC_BSLS, KC_UP,  KC_QUOT, KC_EQL,
-             KC_F11,  KC_F12,  KC_LEFT, KC_DOWN,KC_RGHT, _______,
-            KC_RSFT, KC_APP,  LAYER_0, KC_NO,  KC_NO,   KC_NO );
+ void setupKeymap() {
 
-    /*
-     * add the other layers
-     */
-    for (int row = 0; row < MATRIX_ROWS; ++row)
-    {
-        for (int col = 0; col < MATRIX_COLS; ++col)
-        {
-            matrix[row][col].addActivation(_L1, Method::PRESS, layer1[row][col]);
-        }
-    }
+     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
+     KEYMAP(
+       KC_EQL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,   \
+       KC_TAB,  _______, KC_F12, _______, _______, _______,  _______,  \
+       KC_LCTL,  _______, _______, _______, _______, _______,  _______, \
+       KC_LSFT,  _______, _______, _______, _______, _______,   KC_DEL,  \
+       KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+       KC_ESC,  KC_GRV,   KC_LBRC, KC_RBRC, KC_LSFT, KC_LGUI,   KC_ENT,  \
+     );
 
-    /* 
-     * add special, single activations with the 
-     * layer, activation method and activation
-     */
-    matrix[2][3].addActivation(_QWERTY, Method::MT_TAP, KC_Y);
-    matrix[1][1].addActivation(_QWERTY, Method::MT_TAP, TG(KC_LSHIFT));
-}
+     /*
+      * add the other layers
+      */
+     for (int row = 0; row < MATRIX_ROWS; ++row)
+     {
+         for (int col = 0; col < MATRIX_COLS; ++col)
+         {
+             matrix[row][col].addActivation(_L1, Method::PRESS, layer1[row][col]);
+         }
+     }
+
+ }
 
 
 #endif /* KEYBOARD_SIDE */
-
